85259941809e60b1f780b75a3ad5e43f6feef1d4
ritesh2403 <ghorairitesh75@gmail.com>
2022-08-09 01:30:29 +0530

    Added Animated Icon

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,24 +1,26 @@
 import { Text, HStack, Heading, Box, Flex,Image, useColorModeValue, Stack, Button, ButtonGroup } from '@chakra-ui/react';
 import React from 'react'
 import TypeAnimation from 'react-type-animation';
+import MiniIcon from '../icon/MiniIcon';
 import InfoCard from './InfoCard';
-import Scroller from './Scroller';
 const Hero = () => {
   return (
     <>
     <Flex height='100vh' direction='column' alignItems='center' justifyContent='center'>
     <Stack direction={['column','column','row','row']}>
     <Stack align='center' >
+    <MiniIcon/>  
     <Heading bgGradient='linear(to-l, #7928CA, #FF0080)' bgClip="text">MiniHub</Heading>
     <HStack>
         <Text color={useColorModeValue('gray.600','gray.400')}>A place developed to</Text>
         <TypeAnimation
         cursor={true}
-        sequence={['explore.', 2000, 'discover.', 2000,'contribute.', 2000,'']}
+        sequence={['explore.', 2000, 'discover.', 2000,'contribute.', 2000]}
         wrapper="h2"
         repeat={Infinity}
         />
     </HStack>
+    
     <Flex mt='5' w={'300px'} justifyContent='space-around'>
         <InfoCard number='83' name='Projects'/>
         <InfoCard number='20' name='Contributers'/>
